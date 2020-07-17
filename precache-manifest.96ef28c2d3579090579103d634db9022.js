ed58581eac05c56d2e281975258a59dabfc3dd68
Praveena <praveena180999@gmail.com>
2020-07-17 10:32:28 +0530

    Updates

---
+++ precache-manifest.96ef28c2d3579090579103d634db9022.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a3f11d3de37daf2b87801fb41f258f4",
+    "url": "/BookFactory/index.html"
+  },
+  {
+    "revision": "fc24c3c7ad4189a28ae9",
+    "url": "/BookFactory/static/css/main.a4d94e2c.chunk.css"
+  },
+  {
+    "revision": "eb9be38989b800f4f56f",
+    "url": "/BookFactory/static/js/2.da38207a.chunk.js"
+  },
+  {
+    "revision": "e4d7c18cc4c58d8b735fa1eaf2f02446",
+    "url": "/BookFactory/static/js/2.da38207a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc24c3c7ad4189a28ae9",
+    "url": "/BookFactory/static/js/main.bf0fcce4.chunk.js"
+  },
+  {
+    "revision": "86e0bc984f3c06ededac",
+    "url": "/BookFactory/static/js/runtime-main.7ce974a3.js"
+  }
+]);
